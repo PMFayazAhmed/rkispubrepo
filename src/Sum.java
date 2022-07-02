9f7b5947850ebeb46d560231564124ed96583b3e
Fayaz <fana.fayaz@gmail.com>
2022-07-02 20:33:44 +0530

    Revert "Modified variable values"
    This reverts commit 82cb4f3484df908b1e9535797286d89889d25365.

--- src/Sum.java
+++ src/Sum.java
@@ -2,7 +2,7 @@ class Addiion
 {
 public static void main(String args[])
 {
-	int n1=15,n2=25,total=0;
+	int n1=10,n2=15,total=0;
 	total=n1/n2;
 	System.out.println("Sum of two numbers is :"+total);
 	System.out.println("This line is newly added");
